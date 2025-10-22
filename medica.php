2de70051918cdb95675b005be50a6be1328e66ac
JakubWojtanowski19 <jakub.wojtanowski@zsz.bobowa.pl>
2025-10-22 13:54:11 +0200

    koniec

---
+++ medica.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Przychodnia Medica</title>
+    <link rel="icon" href="obraz2.png" type="image/png">
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h1>Abonamenty w przychodni Medica</h1>
+    </header>
+    <article>
+        <h3>Pakiet Standard - cena 200zł</h3>
+        <p>Zdrowie jest dla Ciebie ważne i nie chcesz przepłacać?.
+            W abonamencie medycznym standardowym nie musisz płacić osobno za każda wizytę,
+            a najczęściej wybierane poradnie masz w cenie pakietu bez skierowań i zbędnych formalności.
+            Korzystaj z wygodnych e-wizyt, zamawiaj recepty przez telefon,
+            umawiaj i odwołuj wizyty w aplikacji mobilnej. Zyskaj dostęp do sieci przychodni w całym Wrocławiu.
+        </p>
+
+        <h3>Pakiet Premium - cena 500zł</h3>
+        <p>Szukasz najwygodniejszego rozwiązania jakie mamy?.
+            Pakiet Premium to dostęp do wszystkich naszych poradni w Wrocławiu.
+        </p>
+
+        <a href="opis.html">Dowiedz się więcej</a>
+    </article>
+    <main>
+        <section>
+            <h2>Standardowy</h2>
+            <ul>
+                <li>Lekarz POZ</li>
+                <li>Endokrynolog</li>
+                <li>Kardiolog</li>
+                <li>Ginekolog</li>
+                <li>Urolog</li>
+                <li>Podstawowe badania laboratoryjne</li>
+            </ul>
+        </section>
+
+        <section>
+            <h2>Premium</h2>
+            <ul>
+                <li>Lekarz POZ</li>
+                <li>Pediatra</li>
+                <li>Geriatra</li>
+                <li>Kardiolog</li>
+                <li>Ortopeda</li>
+                <li>Laryngolog</li>
+                <li>Urolog</li>
+                <li>Endokrynolog</li>
+                <li>Ginekolog</li>
+                <li>Wszystkie badania laboratoryjne</li>
+                <li>Diabetolog</li>
+            </ul>
+        </section>
+
+        <section>
+            <h2>Dziecko</h2>
+            <ul>
+                <li>Pediatra</li>
+                <li>Kardiolog</li>
+                <li>Ortopeda</li>
+                <li>Laryngolog</li>
+                <li>Urolog</li>
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <p>
+            <img src="obraz2.png" alt="przychodnia">
+            Stronę przygotował: 00000000000
+        </p>
+    </footer>
+</body>
+</html>
